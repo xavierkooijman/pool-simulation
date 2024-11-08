dc23ca80de00023c8133a153bb5ff158ee2c4cb6
Xavier Kooijman <xavierkooijman@gmail.com>
2024-11-08 11:00:42 +0000

    basic setup

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Accessing the canvas and context
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+//Global variables
+const width = canvas.width;
+const height = canvas.height;
+
