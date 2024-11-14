b75d25eb75bb60400b0aecbac96759531a0647ae
Xavier Kooijman <xavierkooijman@gmail.com>
2024-11-14 20:34:37 +0000

    update ball velocity and change ball direction on bouncing off of walls

--- js/ball.js
+++ js/ball.js
@@ -1,4 +1,4 @@
-import { ctx } from "./canvas.js"
+import { canvas, canvasMargin, ctx } from "./canvas.js"
 
 export class Ball{
   constructor({pos, color, vel}){
@@ -16,4 +16,31 @@ export class Ball{
     ctx.closePath()
   }
 
+  update(){
+    this.pos.x += this.vel.x
+    this.pos.y += this.vel.y
+    this.bounceOfWall()
+  }
+
+  bounceOfWall(){
+    //horizontal
+    if(this.pos.x + this.size >= canvas.width - canvasMargin){
+      this.pos.x = canvas.width - canvasMargin - this.size
+      this.vel.x *= -1
+    }
+    else if(this.pos.x - this.size <= canvasMargin){
+      this.pos.x = this.size + canvasMargin
+      this.vel.x *= -1
+    }
+
+    //vertical
+    if(this.pos.y + this.size >= canvas.height - canvasMargin){
+      this.pos.y = canvas.height - canvasMargin - this.size
+      this.vel.y *= -1
+    }
+    else if(this.pos.y - this.size <= canvasMargin){
+      this.pos.y = this.size + canvasMargin
+      this.vel.y *= -1
+    }
+  }
 }
