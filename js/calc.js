ce573375ad9304e62ec7a500bd7fa6a81c233813
Xavier Kooijman <xavierkooijman@gmail.com>
2024-11-28 23:30:24 +0000

    vector operations and distance between two points

---
+++ js/calc.js
@@ -0,0 +1,24 @@
+// addition of two vectors
+export function add(v, w) {
+  return { x: v.x + w.x, y: v.y + w.y };
+}
+
+// subtraction of two vectors
+export function sub(v, w) {
+  return { x: v.x - w.x, y: v.y - w.y };
+}
+
+// scale a vector
+export function scale(r, v) {
+  return { x: r * v.x, y: r * v.y };
+}
+
+// distance between two points
+export function distance(p, q) {
+  return Math.sqrt(Math.pow(p.x - q.x, 2) + Math.pow(p.y - q.y, 2));
+}
+
+// dot product of two vectors
+export function dotProduct(v, w) {
+  return v.x * w.x + v.y * w.y;
+}
